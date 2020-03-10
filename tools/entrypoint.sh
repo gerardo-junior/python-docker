f84014cd4d56fba453580a90e514d5b6686ddb27
ghost <ghost@users.noreply.github.com>
2020-03-10 19:42:35 +0000

    anonymous commit

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env $SHELL
 sudo chgrp -Rf ${USER} ${WORKDIR}
 
 if [ -e "$(pwd)/requirements.txt" ]; then
-    /usr/local/bin/pip install --no-cache-dir -r requirements.txt
+    /usr/local/bin/python -m pip install --no-cache-dir -r requirements.txt
 fi
 
-if [ ! -z "$1" ]; then
-    if [ -z "$(which -- $1)" ]; then
-        /usr/local/bin/python "$@"
-    else
-        exec "$@"
-    fi
+if [ ! -z "$(which -- $1)" ]; then
+    exec "$@"
+else
+    /usr/local/bin/python "$@"
 fi
